b8a336ebcfaa559c25029255f199d331fb60a8a7
ianboucher <i.a.boucher@gmail.com>
2017-05-08 14:44:26 +0100

    Add task views & controller actions. Configure Repositories & Policies

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\TaskRepository;
 
 class TasksController extends Controller
 {
+    /**
+     * The task repository instance.
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
     /**
      * Create a new controller instance.
      *
@@ -15,4 +23,50 @@ class TasksController extends Controller
      {
          $this->middleware('auth');
      }
+
+     /**
+      * Display all tasks belonging to user.
+      *
+      * @param  Request  $request
+      * @return Response
+      */
+      public function index(Request $request)
+      {
+          return view('tasks.index', [
+              'tasks' => $this->tasks->forUser($request->user()),
+          ]);
+      }
+
+      /**
+       * Create a new task
+       *
+       * @param  Request  $request
+       * @return Response
+       */
+       public function store(Request $request)
+       {
+           $this->validate($request, [
+               'description' => 'required|max:255',
+           ]);
+
+           $request->user()->tasks()->create([
+               'description' => $request->description,
+           ]);
+
+           return redirect('/tasks');
+       }
+
+       /**
+        * Destroy the specified task
+        *
+        * @param  Request  $request
+        * @param  Task $task
+        * @return Response
+        */
+        public function destroy(Request $request, Task $task)
+        {
+            $this->authorize('destroy', $task);
+            $task->detete();
+            return redirect('/tasks');
+        }
 }
